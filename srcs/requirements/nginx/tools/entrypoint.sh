9c59fe947d929899cd40c202499c4c29ea7a72c5
Eithan ASSOULINE <eassouli@e1r10p4.42.fr>
2022-03-30 20:58:27 +0200

    remove php my admin, start creating table for database, fix wordpress download

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -3,11 +3,7 @@
 # SSL/TLS Config
 mkdir -p /etc/ssl/private
 chmod 700 /etc/ssl/private
-cat "/input-ssl.txt" | openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
-
-# nginx -s start
-# envsubst '\$DOMAIN_NAME \$NGINX_PORT \$NGINX_PORT_SSL \$CERTS_PUB \$CERTS_KEY' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
-# nginx -c /etc/nginx/conf.d/nginx.conf
+cat "/input-ssl.txt" | openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_KEY -out $CERTS_PUB
 
 # Stay on
 nginx -g 'daemon off;'
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,9 +2,8 @@
 
 sleep 5
 # Setup Wordpress
-# wp core config --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306
+# wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306
 # wp core config --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306
 
 # Stay on
 php-fpm7.3 --nodaemonize
-		# tail -f
